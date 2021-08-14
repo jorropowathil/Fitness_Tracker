24e2a7e011462c82eb0e616edee533a2d7f02555
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-08-14 01:18:23 -0400

    Worked on intial files for homework

---
+++ server.js
@@ -0,0 +1,34 @@
+// Required
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+// Port
+const PORT = process.env.PORT || 3000;
+// Routes (edit as needed)
+// const routes = require("routes");
+// const api = require("api")
+// Middleware
+const app = express();
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// View
+app.use(express.static("public"));
+
+// Mongoosedb Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTrackerDB", {
+  useNewUrlParser: true,
+});
+
+// App.use routes (fix when routes established)
+// app.use("route_views");
+// app.use("route_api");
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
