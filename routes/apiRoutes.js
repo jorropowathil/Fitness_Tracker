3b09eeb26d9bee09ed24c9dace8eb30cdd02bac1
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-07 04:17:04 -0400

    some api routes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,11 +4,36 @@ const workout = require("../models/workout.js");
 
 // Api workout route
 router.get("/api/workouts", (req, res) => {
-    
+    Workout.aggregate([{
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration",
+            },
+         },},])
+        .then((workoutsdb) => {
+            res.json(workoutsdb);
+            console.log(workoutsdb);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 
 // Api workout Post route
 router.post("/api/workouts", (req, res) => {
-    
+    Workout.create(req.body)
+        .then((workoutsdb) => {
+            res.json(workoutsdb);
+        })
+        .catch((err) => {
+            console.log(workoutsdb);
+            res.json(err);
+        });
 });
 
+
+
+
+
+// router.get("/api/workouts/range", (req, res) => {
+// });
